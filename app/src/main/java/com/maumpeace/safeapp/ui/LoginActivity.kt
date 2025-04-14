d6ab00fe2937a3d567341ff630ea35ae66f992e8
socical <socical.dev@gmail.com>
2025-04-14 11:18:27 +0900

    chore: splash 이미지 svg 로 변경

--- app/src/main/java/com/maumpeace/safeapp/ui/LoginActivity.kt
+++ app/src/main/java/com/maumpeace/safeapp/ui/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.maumpeace.safeapp.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +29,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // API 30 이상에서 전체화면 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION") window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         setupObservers()
         setupListeners()
